9f1d439f6c106c60ee756cc283ff928d5524de6c
franc <franc@DESKTOP-7GB8H3L.local>
2021-03-25 02:28:15 +0200

    premier connexion avec le repository

---
+++ src/main/java/com/openclassroom/api/ApiApplication.java
@@ -0,0 +1,24 @@
+package com.openclassroom.api;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ApiApplication implements  CommandLineRunner {
+	
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	
+	@Override
+    public void run(String... args) throws Exception {
+		System.out.println("test");
+    }
+}
